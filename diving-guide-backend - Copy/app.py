a1782b5ef8bea30b63b17e74424f1c6c0d6b53d3
Chithmi Dilaksha <148516815+ChithmiDilaksha@users.noreply.github.com>
2025-07-13 12:18:58 +0530

    Project

---
+++ diving-guide-backend - Copy/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+from routes import suggest_diving_areas_endpoint, cdiving, classify_image
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+app.add_url_rule('/suggest_diving_areas', 'suggest_diving_areas', suggest_diving_areas_endpoint, methods=['GET'])
+
+app.add_url_rule('/cdiving', 'cdiving', cdiving, methods=['POST'])
+
+app.add_url_rule('/classify_image', 'classify_image', classify_image, methods=['POST'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ diving-guide-backend - Copy/routes.py
@@ -0,0 +1,43 @@
+from flask import request, jsonify
+from ruindiv import suggest_diving_areas
+from cdiving import preprocess_input, make_prediction
+from seaani import predict_image_from_bytes
+
+def suggest_diving_areas_endpoint():
+    experience_level = request.args.get('experience_level')
+    if not experience_level:
+        return jsonify({'error': 'Experience level is required'}), 400
+    
+    suggestions = suggest_diving_areas(experience_level)
+    if isinstance(suggestions, str):
+        return jsonify({'error': suggestions}), 400
+    
+    return jsonify(suggestions)
+
+def cdiving():
+    try:
+        data = request.json
+        
+        processed_data = preprocess_input(data)
+        
+        predicted_label = make_prediction(processed_data)
+        
+        return jsonify({"score": predicted_label})
+    
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+def classify_image():
+    try:
+        if 'image' not in request.files:
+            return jsonify({"error": "No image file provided"}), 400
+
+        image_file = request.files['image']
+        image_bytes = image_file.read()
+
+        predicted_label = predict_image_from_bytes(image_bytes)
+
+        return jsonify({"Predicted_Label": predicted_label})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
